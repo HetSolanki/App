069d603e11931e915e5e3f55d6ecd0e0f0627907
Dhruv Prajapati <dhruvprajapati66572@gmail.com>
2022-08-06 02:38:44 +0530

    changes

--- WindowsApp1/WindowsApp1/Login_Page.vb
+++ WindowsApp1/WindowsApp1/Login_Page.vb
@@ -3,40 +3,47 @@
         Dim d As New DAO
     End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim d As New DAO
-        Dim obj As SqlClient.SqlDataReader
-        Dim flag As Boolean = False
-        Dim u_type As String
-        obj = d.getData("Select * from Admin where username = '" & TextBox1.Text & "' and password = '" & TextBox2.Text & "'")
-        While obj.Read
-            flag = True
-            u_type = obj.Item("u_type")
-        End While
-
-        d.close_conn()
-
-        If flag Then
-            uname = TextBox1.Text
-            utype = u_type
-            Dim mainpage As New Form1
-            Me.Hide()
+        If TextBox1.Text = "Admin" & TextBox1.Text = "Admin" Then
             lodding.Timer1.Start()
         Else
-            MsgBox("Enter Valid Username And Password")
-            TextBox2.Text = ""
-            TextBox1.Focus()
+            MsgBox("Invalid Id Password")
         End If
+
+
+        'Dim d As New DAO
+        'Dim obj As SqlClient.SqlDataReader
+        'Dim flag As Boolean = False
+        'Dim u_type As String
+        'obj = d.getData("Select * from Admin where username = '" & TextBox1.Text & "' and password = '" & TextBox2.Text & "'")
+        'While obj.Read
+        '    flag = True
+        '    u_type = obj.Item("u_type")
+        'End While
+
+        'd.close_conn()
+
+        'If flag Then
+        '    uname = TextBox1.Text
+        '    utype = u_type
+        '    Me.Hide()
+        '    lodding.Timer1.Start()
+
+        'Else
+        '    MsgBox("Enter Valid Username And Password")
+        '    TextBox2.Text = ""
+        '    TextBox1.Focus()
+        'End If
     End Sub
 
     Private Sub TextBox1_TextChanged(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown, TextBox2.KeyDown, Button1.KeyDown, TextBox1.TextChanged
-        If e.KeyCode = Keys.Enter Then
-            If sender.name = TextBox1.Name Then
-                TextBox2.Focus()
-            ElseIf sender.name = TextBox2.Name Then
-                Button1_Click(sender, e)
-            End If
+        'If e.KeyCode = Keys.Enter Then
+        '    If sender.name = TextBox1.Name Then
+        '        TextBox2.Focus()
+        '    ElseIf sender.name = TextBox2.Name Then
+        '        Button1_Click(sender, e)
+        '    End If
 
-        End If
+        'End If
     End Sub
 
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
